a0ad6c28b11b3e1ed55358186464eb41b6cd2818
EntityNull12 <endaadittia@gmail.com>
2025-02-21 13:22:34 +0700

    style[EN]: add tailwind in navbar and navdown

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,23 +1,23 @@
-<div class="footer-bar-background"></div>
-<div class="footer-links-column">
-    <figure class="footer-link-item">
-        <img class="footer-icon" src="{{ asset('assets/UntitledPage3/bell_icon.png') }}" alt="bell icon" />
-        <figcaption class="footer-link-description">Perlengkapan</figcaption>
+<div class="absolute w-full bottom-0 left-[50%] h-[64px] -translate-x-[50%] bg-white"></div>
+<div class="  absolute top-0 left-[50%] flex justify-between gap-2 md:gap-8 w-full px-4 md:px-0 max-w-[815px] h-20 -translate-x-[50%]">
+    <figure class=" gap-2 mt-7 items-center flex flex-col w-[79px]  ">
+        <img class=" w-6 object-cover" src="{{ asset('assets/UntitledPage3/bell_icon.png') }}" alt="bell icon" />
+        <figcaption class=" text-xs">Perlengkapan</figcaption>
     </figure>
-    <figure class="footer-link-item">
-        <img class="footer-icon" src="{{ asset('assets/UntitledPage3/calendar_icon.png') }}" alt="calendar icon" />
-        <figcaption class="footer-link-description">Pembayaran</figcaption>
+    <figure class=" gap-2 mt-7 items-center flex flex-col w-[79px]  ">
+        <img class=" w-6 object-cover" src="{{ asset('assets/UntitledPage3/calendar_icon.png') }}" alt="calendar icon" />
+        <figcaption class=" text-xs text-black">Pembayaran</figcaption>
     </figure>
-    <figure class="footer-link-item">
-        <img class="footer-icon" src="{{ asset('assets/UntitledPage3/home_icon.png') }}" alt="home icon" />
-        <figcaption class="footer-link-description">Home</figcaption>
+    <figure class=" gap-2 mt-7 items-center flex flex-col w-[79px] relative">
+        <img class=" object-cover z-1 w-14 h-14 rounded-full bg-black absolute top-[-30px]" src="{{ asset('assets/UntitledPage3/home_icon.png') }}" alt="home icon" />
+        <figcaption class=" text-xs text-black mt-8">Home</figcaption>
     </figure>
-    <figure class="footer-link-item">
-        <img class="footer-icon" src="{{ asset('assets/UntitledPage3/camera_icon.svg') }}" alt="camera icon" />
-        <figcaption class="footer-link-description">Dokumen</figcaption>
+    <figure class=" gap-2 mt-7 items-center flex flex-col w-[79px]  ">
+        <img class=" w-6 object-cover" src="{{ asset('assets/UntitledPage3/camera_icon.svg') }}" alt="camera icon" />
+        <figcaption class=" text-xs text-black">Dokumen</figcaption>
     </figure>
-    <figure class="footer-link-item">
-        <img class="footer-icon" src="{{ asset('assets/UntitledPage3/4127a0509d5d7e15ea80e5a3e8619b3a.svg') }}" alt="jamaah icon" />
-        <figcaption class="footer-link-description">Jamaah</figcaption>
+    <figure class=" gap-2 mt-7 items-center flex flex-col w-[79px]  ">
+        <img class=" w-6 object-cover" src="{{ asset('assets/UntitledPage3/4127a0509d5d7e15ea80e5a3e8619b3a.svg') }}" alt="jamaah icon" />
+        <figcaption class=" text-xs text-black">Jamaah</figcaption>
     </figure>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<main class="main-content">
+<main class=" flex flex-col overflow-hidden bg-[#f4f4f4] pb-32">
     <!-- Section untuk header -->
-    <section class="header-gallery">
-        <div class="header-image-row">
+    <section class="bg-white">
+        <div class=" container-sm mx-8 md:mx-auto flex justify-between items-center max-w-[85%] w-[925px]">
             <img class="main-header-image" src="{{ asset('assets/UntitledPage3/e219d07362486683403a2cb083b5ccdf.png') }}" alt="main image" />
             <img class="menu-icon" src="{{ asset('assets/UntitledPage3/menu_icon.svg') }}" alt="menu icon" />
         </div>
     </section>
 
     <!-- Section untuk menampilkan gallery atau produk -->
-    <section class="fles flex-col align-items-center gallery-display mt-14">
+    <section class="fles flex-col align-items-center container-sm mx-8 md:mx-auto mt-14">
         <div class="flex flex-col gap-8">
             <div class="min-h-[240px] min-w-full bg-gray-300 rounded-xl"></div>
-            <div class="flex flex-col lg:flex-row gap-3">
-                <div class="w-[460px] min-h-[126px] bg-gray-300 rounded-xl"></div>
-                <div class="w-[460px] min-h-[126px] bg-gray-300 rounded-xl"></div>
+            <div class="flex flex-col md:flex-row gap-3">
+                <div class="w-full md:w-[460px] min-h-[126px] bg-gray-300 rounded-xl"></div>
+                <div class="w-full md:w-[460px] min-h-[126px] bg-gray-300 rounded-xl"></div>
             </div>
             <div class="min-h-[240px] min-w-full bg-gray-300 rounded-xl"></div>
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 ">
-                <div class="w-full lg:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
-                <div class="w-full lg:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
-                <div class="w-full lg:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
-                <div class="w-full lg:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
-                <div class="w-full lg:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
-                <div class="w-full lg:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-8 ">
+                <div class="w-full md:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
+                <div class="w-full md:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
+                <div class="w-full md:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
+                <div class="w-full md:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
+                <div class="w-full md:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
+                <div class="w-full md:w-[260px] min-h-[126px] bg-gray-300 rounded-xl"></div>
             </div>
         </div>
     </section>
 
     <!-- Section untuk sticky footer navigation -->
     <section class="footer-navbar">
-        <div class="footer-column">
+        <div class="footer-colum mt-14 min-h-20 w-full fixed bottom-0">
             @include('partials.footer') <!-- Meng-include footer di sini -->
         </div>
     </section>
